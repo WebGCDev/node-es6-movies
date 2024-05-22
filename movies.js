4d1a95db8f3f99fd74a1ce9df6737b3add449d15
WebGCDev <gaetano.chi@gmail.com>
2024-05-22 14:56:46 +0200

    Add: 1) Array di oggetti movies.js

---
+++ movies.js
@@ -0,0 +1,41 @@
+// Definire un array di oggetti; ogni oggetto rappresenta un film o serie tv, che è caratterizzato da: title, year, genre, rating, type (movie o tv), seasons (solo per serie tv).
+
+const data = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    genre: 'Drama',
+    rating: 10,
+    type: 'movie',
+  },
+  {
+    title: 'Person of Interest',
+    year: 2008,
+    genre: 'Crime drama',
+    rating: 9.7,
+    type: 'tv',
+    seasons: 5,
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    genre: 'Sci-Fi',
+    rating: 8.8,
+    type: 'movie',
+  },
+  {
+    title: 'Game of Thrones',
+    year: 2011,
+    genre: 'Fantasy',
+    rating: 9.3,
+    type: 'tv',
+    seasons: 8,
+  },
+  {
+    title: 'The Matrix',
+    year: 1999,
+    genre: 'Sci-Fi',
+    rating: 8.7,
+    type: 'movie',
+  },
+];
